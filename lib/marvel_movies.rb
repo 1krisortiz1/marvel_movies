d25564b878309e823ad27a638fcd80dc23b8eff2
1krisortiz1 <itskristin.ortiz@gmail.com>
2020-10-11 15:31:58 -0700

    updated gemfile

--- lib/marvel_movies.rb
+++ lib/marvel_movies.rb
@@ -1,6 +1,8 @@
 require_relative "./marvel_movies/version"
 require_relative "./marvel_movies/cli"
 
+require 'pry'
+
 module MarvelMovies
   class Error < StandardError; end
   # Your code goes here...
--- lib/marvel_movies/cli.rb
+++ lib/marvel_movies/cli.rb
@@ -3,6 +3,15 @@ class MarvelMovies::CLI
   #call cli
   def call
     puts "Welcome to Marvel Movies"
+    binding.pry
+    order
+  end
+
+  def order
+    puts "Select an order:"
+    puts "list one"
+    puts "list two"
+    puts "list three"
   end
 
 end
