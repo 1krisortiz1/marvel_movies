94ca6db69a384f8f145a9eec1e2dfdd94e8464c3
1krisortiz1 <itskristin.ortiz@gmail.com>
2020-10-11 17:09:59 -0700

    added get_orders and get_user_order_choice

--- lib/marvel_movies/cli.rb
+++ lib/marvel_movies/cli.rb
@@ -3,15 +3,19 @@ class MarvelMovies::CLI
   #call cli
   def call
     puts "Welcome to Marvel Movies"
-    binding.pry
-    order
+    get_orders
+    get_user_order_choice
+    #get_order(choice)
+    #list_
   end
 
-  def order
-    puts "Select an order:"
-    puts "list one"
-    puts "list two"
-    puts "list three"
+  def get_orders
+    @orders = ['chronological order', 'release date']
+  end
+
+  def get_user_order_choice
+    puts "Make a selection:"
+    @orders.each_with_index { |choice, index| puts "#{index + 1}. #{choice}" }
   end
 
 end
