fa7bea0c4d51dc474b5d323a5e3c43327a5e35fb
1krisortiz1 <itskristin.ortiz@gmail.com>
2020-10-11 17:39:12 -0700

    added validation check

--- lib/marvel_movies/cli.rb
+++ lib/marvel_movies/cli.rb
@@ -2,9 +2,10 @@ class MarvelMovies::CLI
 
   #call cli
   def call
-    puts "Welcome to Marvel Movies"
+    puts "\nWelcome to Marvel Movies"
     get_orders
-    get_user_order_choice
+    list_choices
+    get_user_choice
     #get_order(choice)
     #list_
   end
@@ -13,9 +14,18 @@ class MarvelMovies::CLI
     @orders = ['chronological order', 'release date']
   end
 
-  def get_user_order_choice
-    puts "Make a selection:"
-    @orders.each_with_index { |choice, index| puts "#{index + 1}. #{choice}" }
+  def list_choices
+    puts "\nMake a selection:"
+    @orders.each.with_index(1) { |choice, index| puts "#{index}. #{choice}" }
+  end
+
+  def get_user_choice
+    choice = gets.strip 
+    #if valid_choice(choice, @orders)
+  end
+
+  def valid_choice(input, data)
+    input.to_i <= data.length && input.to_i > 0
   end
 
 end
